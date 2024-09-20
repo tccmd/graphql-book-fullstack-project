a05d8439baf7c2c9a5682fcce50fa86e924c6898
tccmd <tccmd52@gmail.com>
2024-09-20 22:33:21 +0900

    필름 리스트, 카드 컴포넌트

--- project/server/src/entities/Director.ts
+++ project/server/src/entities/Director.ts
@@ -3,8 +3,16 @@ import { ObjectType, Field, Int } from "type-graphql";
 @ObjectType()
 export class Director {
   @Field(() => Int)
-  id!: number
+  id: number
 
   @Field(() => String)
-  name!: string
+  name: string
+
+  constructor(
+    id: number, 
+    name:string
+  ) {
+    this.id = id;
+    this.name = name;
+  }
 }
--- project/server/src/resolvers/Film.ts
+++ project/server/src/resolvers/Film.ts
@@ -1,7 +1,17 @@
-import { Query, Resolver, FieldResolver, Root } from "type-graphql";
+import { Query, Resolver, FieldResolver, Root, ObjectType, Field, Int } from "type-graphql";
 import ghibliData from "../data/ghibli";
 import { Film } from "../entities/Film";
 import { Director } from "../entities/Director";
+
+// 페이지 처리된 영화 목록 반환 오브젝트 타입
+@ObjectType()
+class PaginatedFilms {
+  @Field(() => [Film])
+  films!: Film[]
+
+  @Field(() => Int, { nullable: true })
+  cursor?: Film[ 'id' ] | null;
+}
 
 @Resolver(Film)
 export class FilmResolver {
